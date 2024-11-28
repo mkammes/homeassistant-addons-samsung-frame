3cabcab4719292aea4a99ae0ad59b357609da350
Gijs van den Hoven <gijs@vd-hoven.net>
2024-11-28 19:48:32 +0100

    Refactor art upload script to use asynchronous API and update Dockerfile to install Pillow

--- homeassistant-samsung-frame-art-beta/art.py
+++ homeassistant-samsung-frame-art-beta/art.py
@@ -1,100 +1,98 @@
+#!/usr/bin/env python3
+# NOTE old api is 2021 and earlier Frame TV's, new api is 2022+ Frame TV's
+
 import os
-import time
-from samsungtvws import SamsungTVWS
+import asyncio
+import logging
 import argparse
 
-parser = argparse.ArgumentParser(description='Upload images to Samsung TV.')
-parser.add_argument('--tvip', help='IP address of the Samsung the Frame')
+from samsungtvws.async_art import SamsungTVAsyncArt
+from samsungtvws import exceptions
 
-args = parser.parse_args()
+logging.basicConfig(level=logging.INFO) #or logging.DEBUG to see messages
 
-IMAGE_DIRECTORY = "/media/frame"
+def parseargs():
+    # Add command line argument parsing
+    parser = argparse.ArgumentParser(description='Example async art Samsung Frame TV.')
+    parser.add_argument('ip', action="store", type=str, default=None, help='ip address of TV (default: %(default)s))')
+    return parser.parse_args()
+    
+async def image_callback(event, response):
+    logging.info('CALLBACK: image callback: {}, {}'.format(event, response))
 
-tvip = args.tvip
+async def main():
+    args = parseargs()
+    tv = SamsungTVAsyncArt(host=args.ip, port=8002)
+    await tv.start_listening()
+    
+    #is art mode supported
+    supported = await tv.supported()
+    logging.info('art mode is supported: {}'.format(supported))
+    
+    if supported:
+        try:
+            #is tv on (calls tv rest api)
+            tv_on = await tv.on()
+            logging.info('tv is on: {}'.format(tv_on))
+            
+            #is art mode on
+            #art_mode = await tv.get_artmode()                  #calls websocket command to determine status
+            art_mode = tv.art_mode                              #passive, listens for websocket messgages to determine art mode status
+            logging.info('art mode is on: {}'.format(art_mode))
+            
+            #check both with one call (calls tv rest api)
+            logging.info('tv is on and in art mode: {}'.format(await tv.is_artmode()))
+            
+            #get api version 4.3.4.0 is new api, 2.03 is old api
+            api_version = await tv.get_api_version()
+            logging.info('api version: {}'.format(api_version))
+            
 
-def upload_and_display_image(image_path):
-    try:
-        # Connect to the TV
-        tv = SamsungTVWS(host=tvip)
-        art = tv.art()
+            
+            # Request list of all art
+            try:
+                info = await tv.available()
+                #info = await tv.available('MY-C0002')              #gets list of uploaded art, MY-C0004 is favourites
+            except AssertionError:
+                info='None'
+            logging.info('artwork available on tv: {}'.format(info))
 
-        # Read the image
-        with open(image_path, "rb") as f:
-            image_data = f.read()
+            
+            #or get slideshow status, old api fails silently on new TV's, but throws AssertionError on older ones
+            try:
+                info = await tv.get_auto_rotation_status()
+            except AssertionError:
+                info = await tv.get_slideshow_status()
+            logging.info('current slideshow status: {}'.format(info))
+            
+            
+            #upload file
+            '''
+            filename = "framed_IMG_0181.png"
+            content_id = None
+            if filename:
+                with open(filename, "rb") as f:
+                    file_data = f.read()
+                file_type = os.path.splitext(filename)[1][1:] 
+                content_id = await tv.upload(file_data, file_type=file_type)
+                content_id = os.path.splitext(content_id)[0]    #remove file extension if any (eg .jpg)
+                logging.info('uploaded {} to tv as {}'.format(filename, content_id))
+                
+            #delete art on tv
+            if content_id:
+                await tv.delete_list([content_id])
+                logging.info('deleted from tv: {}'.format([content_id]))
+            '''
+            
+            
+            
+            await asyncio.sleep(15)
+        except exceptions.ResponseError as e:
+            logging.warning('ERROR: {}'.format(e))
+        except AssertionError as e:
+            logging.warning('no data received: {}'.format(e))
 
-        print(f"Now Uploading {image_path}...")
-        # Upload the image
-        response = art.upload(
-            image_data,
-            file_type="JPEG",
-            matte="modern_apricot"  # Adjust matte and color as needed
-        )
-        print(f"Uploaded {image_path}: {response}")
+    await tv.close()
 
-        # Assuming the response is the image ID directly
-        image_id = response  # Directly use the string if it's the ID
-        print("Activating Art Mode and displaying the uploaded image...")
-        art.select_image(image_id, show=False)  # Pass the image ID to set it in Art Mode
-        tv.send_key("KEY_POWERON")  # Ensure the TV is ON
-        time.sleep(2)  # Give time for the TV to process
 
-        print("Art Mode activated with the uploaded image.")
-        print(f"{time.ctime()}")
-    except Exception as e:
-        print(f"Error: {e}")
-
-def upload_images_in_directory(directory):
-    # Connect to the TV
-    tv = SamsungTVWS(host=tvip)
-    art = tv.art()
-
-    # Get the list of existing images on the TV
-    existing_images = tv.art().available()
-    logging.info(info)
-
-    # List all .jpg files in the directory
-    files = [filename for filename in os.listdir(directory) if filename.lower().endswith(".jpg")]
-
-    # Filter out images that are already on the TV
-    files_to_upload = [f for f in files if f not in existing_images]
-
-    # Upload all images that are not already on the TV
-    for image in files_to_upload:
-        full_path = os.path.join(directory, image)
-        upload_and_display_image(full_path)
-        # Sleep 2 seconds to let the Frame TV process the image
-        time.sleep(2)
-def display_random_image():
-    try:
-        # Connect to the TV
-        tv = SamsungTVWS(host=tvip)
-        art = tv.art()
-
-        # Get the list of existing images on the TV
-        existing_images = tv.art().available()
-
-        if not existing_images:
-            print("No images available on the TV.")
-            return
-
-        # Randomly select an image
-        selected_image = random.choice(existing_images)
-        print(f"Randomly selected image: {selected_image}")
-
-        # Display the selected image
-        art.select_image(selected_image)  # Pass the image ID to set it in Art Mode
-        tv.send_key("KEY_POWERON")  # Ensure the TV is ON
-        time.sleep(2)  # Give time for the TV to process
-
-        print("Art Mode activated with the randomly selected image.")
-        print(f"{time.ctime()}")
-    except Exception as e:
-        print(f"Error: {e}")
-
-# Call the function to upload and display images
-upload_images_in_directory(IMAGE_DIRECTORY)
-
-display_random_image()
-
-info = tv.art().get_photo_filter_list()
-logging.info(info)
+asyncio.run(main())
--- homeassistant-samsung-frame-art-beta/run.sh
+++ homeassistant-samsung-frame-art-beta/run.sh
@@ -4,7 +4,7 @@ TVIP=$(bashio::config 'tv')
 
 mkdir -p /media/frame
 echo "Using ${TVIP} as the IP of the Samsung Frame"
-python3 art.py --tvip ${TVIP}
+python3 art.py --ip ${TVIP}
 echo "done, closing now!"
 kill -s SIGHUP 1
 
