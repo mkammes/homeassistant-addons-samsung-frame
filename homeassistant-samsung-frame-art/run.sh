5da8cba255640e9f63eea1e464b9557919703d40
Michael K <michael@michaelkammes.com>
2024-11-30 15:46:42 -0800

    Update configuration and scripts for Samsung Frame Art: enable initialization, enhance Dockerfile with improved package installation, and refactor run script to utilize new configuration parameters.

--- homeassistant-samsung-frame-art/run.sh
+++ homeassistant-samsung-frame-art/run.sh
@@ -1,13 +1,11 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bashio
+set -e
 
-TVIP=$(bashio::config 'tv')
-FILTER=$(bashio::config 'filter')
-MATTE=$(bashio::config 'matte')
-MATTE_COLOR=$(bashio::config 'matte_color')
+# Get config values
+TV_IP=$(bashio::config 'tv_ip')
+IMAGE_FOLDER=$(bashio::config 'image_folder')
+IMAGE_PATH=$(bashio::config 'image_path')
 
-mkdir -p /media/frame
-echo "Using ${TVIP} as the IP of the Samsung Frame"
-python3 art.py --ip ${TVIP} --filter ${FILTER} --matte ${MATTE} --matte-color ${MATTE_COLOR}
-echo "done, closing now!"
-kill -s SIGHUP 1
+# Run the Python script
+python3 /run.py
 
